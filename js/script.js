f8a6e21a80e15ab6e02a5fd270155f47ba6d6005
unknown <Artem@mail.ru>
2022-05-31 12:32:01 +0300

    Second practik

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
 
 
 
@@ -10,15 +10,29 @@ const personFilmsDB = {
     actors: {},
     genres: [],
     private: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Какой последний фильм Вы посмотрели"),
+          b = prompt("На сколько Вы его оцените:");
+    if (a.length < 50 && a !== '' && a != null && b !== '' && b != null ){
+        personFilmsDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
 }
 
-const movies = {
-    lastFilm: "",
-    raiting: ""
+if (personFilmsDB.count <= 10) {
+    alert('Вы посмотрели мало фильмов');
+} else if ( numberOfFilms > 10 && numberOfFilms < 30){
+    alert('Вы посмотрели много фильмов');
+} else if ( numberOfFilms > 30) {
+    alert('Вы киноман');
+} else {
+    console.log('error')
 }
 
-movies.lastFilm = prompt("Какой последний фильм Вы посмотрели");
-movies.raiting = prompt("На сколько Вы его оцените:")
-
-
-console.log(movies);
+console.log(personFilmsDB);

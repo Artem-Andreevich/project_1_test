4820a4b8e1ffb7905c4e69e9ff7d88302120b61b
unknown <Artem@mail.ru>
2022-05-31 14:22:45 +0300

    practic lesson #3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 "use strict"
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
-
-
+let numberOfFilms;
 
 const personFilmsDB = {
     count: numberOfFilms,
@@ -12,27 +10,52 @@ const personFilmsDB = {
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Какой последний фильм Вы посмотрели"),
-          b = prompt("На сколько Вы его оцените:");
-    if (a.length < 50 && a !== '' && a != null && b !== '' && b != null ){
-        personFilmsDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+// start();
+// rememberMyFilms();
+// detectPersonalLevel();
+
+function writeYourGenres(arr) {
+    for ( let i = 1; i <= 3; i++) {
+        personFilmsDB.genres[ i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`);
     }
+}
+writeYourGenres(personFilmsDB.genres)
 
+function showDB(arg){
+    (arg == false) ? console.log(personFilmsDB) : console.log('Это приватные настройки')
+}
+showDB(personFilmsDB.private);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Какой последний фильм Вы посмотрели"),
+            b = prompt("На сколько Вы его оцените:");
+        if (a.length < 50 && a !== '' && a != null && b !== '' && b != null ){
+            personFilmsDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
 
-if (personFilmsDB.count <= 10) {
-    alert('Вы посмотрели мало фильмов');
-} else if ( numberOfFilms > 10 && numberOfFilms < 30){
-    alert('Вы посмотрели много фильмов');
-} else if ( numberOfFilms > 30) {
-    alert('Вы киноман');
-} else {
-    console.log('error')
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
+    while ( numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
+    }
 }
 
-console.log(personFilmsDB);
+function detectPersonalLevel() {
+    if (personFilmsDB.count <= 10) {
+        alert('Вы посмотрели мало фильмов');
+    } else if ( numberOfFilms > 10 && numberOfFilms < 30){
+        alert('Вы посмотрели много фильмов');
+    } else if ( numberOfFilms > 30) {
+        alert('Вы киноман');
+    } else {
+        console.log('error')
+    }
+}
+
